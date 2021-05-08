d629f4a45d89c9bfc556bdc1bf21ed8d5ecf7f9e
jixubin <jxb051019@163.com>
2021-05-08 11:52:31 +0800

    lodash按需加载实现.2

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    [
+      'import',
+      {
+        libraryName: 'lodash',
+        libraryDirectory: '',
+        camel2DashComponentName: false, // default: true
+      },
+      'lodash',
+    ],
+  ],
+};
